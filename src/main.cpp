84184bc918727e8d81106f9aff00e77dccad51d6
KsenZ <aksenzov@gmail.com>
2014-05-23 19:42:24 +0800

    Add QDebug output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <QTranslator>
 #include <QLocale>
 #include <QString>
+#include <QDebug>
 #include "QXNeur.h"
 
 int main(int argc, char** argv)
@@ -9,7 +10,8 @@ int main(int argc, char** argv)
 
     QString fname = QString("qxneur_%1.qm").arg(QLocale::system().name().left(2));
     QTranslator translator;
-    translator.load(fname, TRANSLATIONS_DIR, "_");
+    qDebug() << "TRANSLATIONS_DIR: Loading translation file" << fname << "from dir" << TRANSLATIONS_DIR;
+    qDebug() << "load success:" <<translator.load(fname, TRANSLATIONS_DIR, "_");
     app.installTranslator(&translator);
 
     return app.exec();
